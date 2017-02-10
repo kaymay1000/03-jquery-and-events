e14495275279a6f92cc560267f309ed5f1afe60a
Kaylee Alvarado <kaymay1000@gmail.com>
2017-02-09 17:44:31 -0800

    All except for stretch goal/teasers complete

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -9,6 +9,8 @@ articleView.populateFilters = function() {
     $('#author-filter').append(optionTag);
     category = $(this).attr('data-category');
     optionTag = '<option value="' + category + '">' + category + '</option>';
+    // If the category exists, do not append a duplicate <option> tag.
+    // If the category does not exist, append an <option> tag.
     if ($('#category-filter option[value="' + category + '"]').length === 0) {
       $('#category-filter').append(optionTag);
     }
@@ -17,38 +19,56 @@ articleView.populateFilters = function() {
 
 articleView.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
-    if ($(this).val()) {
-      /* TODO: If the select box changes to an option that has a value, we should:
+    if ($(this).val()) { //If the selected element has value, then...
+      /* DONE: If the select box changes to an option that has a value, we should:
           1. Hide all of the articles
           2. Fade in only the articles that match based on on the author
             that was aselected. Hint: use an attribute selector to find
             those articles that match the value, and then fade them in.
         */
-    } else {
+      $('article').hide();
+      $('article[data-author="' + $(this).val() + '"]' ).fadeIn();
+    } else { //If the selected element has no value (i.e. --Filter by Author-- option), then...
     /* Otherwise, we should:
         1. Show all the articles except the template */
+      $('article.template').hide();
+      $('article').fadeIn();
     }
     $('#category-filter').val('');
   });
 };
 
 articleView.handleCategoryFilter = function() {
-  /* TODO: Just like we do for #author-filter above, we should also handle
+  /* DONE: Just like we do for #author-filter above, we should also handle
   change events on the #category-filter element. Be sure to reset the
   #author-filter while you're at it! */
+  $('#category-filter').on('change', function() {
+    if ($(this).val()) {
+      $('article').hide();
+      $('article[data-category="' + $(this).val() + '"]').fadeIn();
+    }
+    else {
+      $('article.template').hide();
+      $('article').fadeIn();
+    }
+    $('#author-filter').val('');
+  })
 };
 
 articleView.handleMainNav = function () {
   $('.main-nav').on('click', '.tab', function() {
-    /* TODO:
+    /* DONE:
       1. Hide all of the .tab-content sections
       2. Fade in the single .tab-content section that is
         associated with the .tab element's data-content attribute.
     */
+    $('.tab-content').hide();
+    $('#' + $(this).data('content')).fadeIn();
   });
   $('.main-nav .tab:first').click();
 };
 
+//All of this is a stretch goal!
 articleView.setTeasers = function() {
   // Truncate logic to show only first two elements within the article body.
   $('.article-body *:nth-of-type(n+2)').hide();
@@ -62,4 +82,8 @@ articleView.setTeasers = function() {
   */
 };
 
-// TODO: Invoke all of the above functions (I mean, methods!):
+// DONE: Invoke all of the above functions (I mean, methods!):
+articleView.populateFilters();
+articleView.handleAuthorFilter();
+articleView.handleCategoryFilter();
+articleView.handleMainNav();
